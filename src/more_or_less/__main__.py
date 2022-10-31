a9648288ec5f4ae5166e4498e181de60200cb002
Nakou <alain.krzeczowski@gmail.com>
2022-10-31 17:10:23 +0100

    #7 Update scoreboard_path and fix erase_scoreboard function

--- src/more_or_less/__main__.py
+++ src/more_or_less/__main__.py
@@ -8,7 +8,7 @@ import typer
 from . import game_scoreboard, game, erase_player_score
 
 app = typer.Typer()
-SCOREBOARD_PATH = "scoreboard.json"
+SCOREBOARD_PATH = "tmp/more_or_less/local_scoreboard.json"
 
 
 @app.command()
@@ -16,9 +16,7 @@ def erase_scoreboard(scoreboard_path: pathlib.Path = SCOREBOARD_PATH):
     """
     More or less Erase Scoreboard
     """
-    erase_player_score(player_name=str,
-                       player_score=int,
-                       scoreboard_path=scoreboard_path)
+    erase_player_score(scoreboard_path=scoreboard_path)
 
 
 @app.command()
